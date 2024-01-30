f0197b4f21e983c7c5113ae44b67089951ae392f
Chris-Haj <chris1191@gmail.com>
2024-01-31 01:20:42 +0200

    Initial commit

---
+++ Ex1/main.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    A = cv.imread('A.jpg')
+    B = cv.imread('B.jpg')
+    C = cv.imread('C.jpg')
