37aa953699bfae9f834f5b4bb30916956ac41b85
Chris-Haj <chris1191@gmail.com>
2024-02-15 00:33:42 +0200

    Finished

--- Ex1/main.py
+++ Ex1/main.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def applyOtsuThresh(Image):
+    # hist_eq_image = cv.equalizeHist(Image)
     bilnearImage = Image
     for i in range(10):
         bilnearImage = cv.resize(bilnearImage, None, None, 0.5, 0.5, cv.INTER_LINEAR)
@@ -16,7 +17,6 @@ def applyOtsuThresh(Image):
         bilnearImage = cv.resize(bilnearImage, None, None, 2, 2, cv.INTER_LINEAR)
 
     thresh, newImage = cv.threshold(bilnearImage, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-
     ComparedImages = np.hstack((Image, newImage))
 
     return ComparedImages
@@ -27,9 +27,22 @@ if __name__ == '__main__':
     imgB = cv.imread('B.jpg', cv.IMREAD_GRAYSCALE)
     imgC = cv.imread('C.jpg', cv.IMREAD_GRAYSCALE)
 
+    heightA, widthA = imgA.shape
+    heightB, widthB = imgB.shape
+    heightC, widthC = imgC.shape
+
+    avg_height = int((heightA + heightB + heightC) / 3)
+    avg_width = int((widthA + widthB + widthC) / 3)
+
+    imgA = cv.resize(imgA, (avg_width, avg_height))
+    imgB = cv.resize(imgB, (avg_width, avg_height))
+    imgC = cv.resize(imgC, (avg_width, avg_height))
+
     otsuA, otsuB, otsuC = applyOtsuThresh(imgA), applyOtsuThresh(imgB), applyOtsuThresh(imgC)
-    cv.imwrite('newA.jpg', otsuA)
-    cv.imwrite('newB.jpg', otsuB)
-    cv.imwrite('newC.jpg', otsuC)
+    AllComparisons = np.vstack((otsuA, otsuB, otsuC))
+    AllComparisons = cv.resize(AllComparisons, None, None, 0.5, 0.5, cv.INTER_LINEAR)
+    cv.imwrite('A_B_C Before and After.jpg', AllComparisons)
+    cv.imshow('A_B_C Before and After', AllComparisons)
 
-
+cv.waitKey(0)
+cv.destroyAllWindows()
